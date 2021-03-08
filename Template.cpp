fb879c4fe06f00f4690afea22748322e98205f4a
Chaudhary Hamdan <54744843+hamdan-codes@users.noreply.github.com>
2021-03-08 22:15:46 +0530

    Policy Based Code Pushed

--- Template.cpp
+++ Template.cpp
@@ -1,21 +1,28 @@
-#include<bits/stdc++.h>
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<cstring>
-#include<string>
-#include<list>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<map>
-#include<unordered_map>
-#include<set>
-
-//#include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/exception.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+#include <ext/pb_ds/list_update_policy.hpp>
+#include <ext/pb_ds/tag_and_trait.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
 
 #define ff              first
 #define ss              second
@@ -36,9 +43,9 @@ using namespace std;
 #define mk(arr,n,type)  type *arr=new type[n];
 #define w(x)            int x; cin>>x; while(x--)
 #define f(i,x,y,inc)    for(int i = x; i < y; i += inc)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-//mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
-//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 
 void c_p_p()
