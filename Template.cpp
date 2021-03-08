ea77fdf88301167f9c827db66b32f6da601f9006
Chaudhary Hamdan <54744843+hamdan-codes@users.noreply.github.com>
2021-03-08 21:15:28 +0530

    Update CPP Template

--- Template.cpp
+++ Template.cpp
@@ -1,7 +1,20 @@
 #include<bits/stdc++.h>
-//#include <ext/pb_ds/assoc_container.hpp>
 #include<iostream>
+#include<algorithm>
+#include<string>
+#include<cstring>
+#include<string>
+#include<list>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+#include<unordered_map>
+#include<set>
+
+//#include <ext/pb_ds/assoc_container.hpp>
 //using namespace __gnu_pbds;
+
 using namespace std;
 
 #define ff              first
@@ -19,11 +32,12 @@ using namespace std;
 #define mod             1000000007
 #define inf             1e18
 #define ps(x,y)         fixed<<setprecision(y)<<x
+#define endl            "\n"
 #define mk(arr,n,type)  type *arr=new type[n];
 #define w(x)            int x; cin>>x; while(x--)
 #define f(i,x,y,inc)    for(int i = x; i < y; i += inc)
-//mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+//mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 
@@ -35,12 +49,18 @@ void c_p_p()
 	freopen("C:\\Users\\KIIT\\output.txt", "w", stdout);
 #endif
 }
+//--------------------------- Your Functions Below ----------------------------------------------
+
+
+
+
+
+
 
 int32_t main()
 {
 	c_p_p();
-	//----------------------------- Your Code Below ------------------------------------------------
-
+	//--------------------------- Your Code Below -----------------------------------------------
 
 
 
