36d803b07442df2e32bc27180643ed0253024131
Chaudhary Hamdan <54744843+hamdan-codes@users.noreply.github.com>
2021-03-12 15:00:00 +0530

    Cpp Template Update

--- Template.cpp
+++ Template.cpp
@@ -1,6 +1,19 @@
+// Author Chaudhary Hamdan
+// Github link : https://github.com/hamdan-codes
+
+
+/* 
+Template available at my Github
+Repository Link :-
+https://github.com/hamdan-codes/templates-for-CP/
+*/
+
+
+
 #include <bits/stdc++.h>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 #include <string>
 #include <cstring>
 #include <string>
@@ -11,6 +24,7 @@
 #include <map>
 #include <unordered_map>
 #include <set>
+
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/exception.hpp>
 #include <ext/pb_ds/hash_policy.hpp>
@@ -18,11 +32,6 @@
 #include <ext/pb_ds/tag_and_trait.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/trie_policy.hpp>
-
-using namespace std;
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-
 
 #define ff              first
 #define ss              second
@@ -37,14 +46,20 @@ using namespace __gnu_cxx;
 #define setbits(x)      __builtin_popcountll(x)
 #define zrobits(x)      __builtin_ctzll(x)
 #define mod             1000000007
-#define inf             1e18
+#define inf             LONG_LONG_MAX
 #define ps(x,y)         fixed<<setprecision(y)<<x
 #define endl            "\n"
-#define mk(arr,n,type)  type *arr=new type[n];
+#define mk(arr,n,type)  type *arr=new type[n]
 #define w(x)            int x; cin>>x; while(x--)
 #define f(i,x,y,inc)    for(int i = x; i < y; i += inc)
-mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+
+using namespace std;
+
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
 
 
@@ -68,6 +83,9 @@ int32_t main()
 {
 	c_p_p();
 	//--------------------------- Your Code Below -----------------------------------------------
+
+
+
 
 
 
