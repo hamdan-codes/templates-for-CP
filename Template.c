518f1818a0e381924d3468e39374441f23a37228
Chaudhary Hamdan <54744843+hamdan-codes@users.noreply.github.com>
2021-07-12 12:27:12 +0530

    C Template Push

---
+++ Template.c
@@ -0,0 +1,59 @@
+/*
+ * Author               : Chaudhary Hamdan
+ * Github link          : https://github.com/hamdan-codes
+ * My Algo Lab Repo     : https://github.com/hamdan-codes/Algo-lab-2021
+ * My Networks Lab Repo : https://github.com/hamdan-codes/Networks-lab-2021
+ */
+
+#include<stdio.h>
+
+#define sf(x)              scanf("%d", &x)
+#define pf(x)              printf("%d ", x)
+#define pfn(x)             printf("%d\n", x)
+#define pfc(x)             printf("%d, ", x)
+#define f(i,x,y)           for(int i = x; i < y; i++)
+#define fi(i,x,y,inc)      for(int i = x; i < y; i+=inc)
+#define rf(i,x,y)          for(int i = x; i >= y; i--)
+
+
+void c_() {
+
+#ifndef ONLINE_JUDGE
+	freopen("C:\\Users\\KIIT\\input", "r", stdin);
+	freopen("C:\\Users\\KIIT\\output", "w", stdout);
+#endif
+}
+
+
+/* ******************************* Your Functions Below ******************************* */
+
+
+
+
+
+
+
+/* ******************************* Your Functions Above ******************************* */
+
+int main() {
+
+	c_();
+
+	/* ***************************** Your Code Below **************************** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/* ***************************** Your Code Above **************************** */
+
+	return 0;
+}
